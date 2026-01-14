00461e452da61fd16485c088580709d21f3343ba
heryan <heryandejesus@gmail.com>
2026-01-14 15:44:52 -0300

    Fix Vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || '/',
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'github' ? '/proyecto-react/' : '/',
+    plugins: [react()],
+  }
 })
