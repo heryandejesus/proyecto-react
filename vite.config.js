75b6bcadc3d55ede7a5565f4f9c70cc7993b5d85
heryan <heryandejesus@gmail.com>
2026-01-14 15:56:15 -0300

    arreglo tailwind desde vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({ mode }) => {
   return {
     base: mode === 'github' ? '/proyecto-react/' : '/',
-    plugins: [react()],
+    plugins: [
+      react(),
+      tailwindcss()
+    ],
   }
 })
+
